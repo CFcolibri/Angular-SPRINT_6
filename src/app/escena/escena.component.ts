8c3860d069e50f42e745ee021889bc2e1793d525
CFcolibri <carlotafigueras@gmail.com>
2023-05-12 16:26:46 +0200

    Exercise4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -7,6 +7,25 @@ import { HistoryItem } from '../Interface/history.interface';
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
-  @Input()//input per rebre del pare
+  @Input()//input per rebre del
   public historiesList: HistoryItem[] = [];
+  public currentSentence = 0;
+
+  prev() {
+    if (this.currentSentence > 0) {
+      this.currentSentence--;
+    }
+  }
+
+  next() {
+    if (this.currentSentence < this.historiesList.length - 1) {
+      this.currentSentence++;
+    }
+  }
+
+  isCurrentSentence(index: number) {
+    return index === this.currentSentence;
+  }
+
 }
+
